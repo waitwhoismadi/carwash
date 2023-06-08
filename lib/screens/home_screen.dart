2b9a086ee781408eadf01734531b8e03805f1c5f
Chinaski <madi.baidolda@gmail.com>
2023-06-08 23:13:36 +0600

    asds

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:taxi_app/bloc/taxi_booking_bloc.dart';
-import 'package:taxi_app/bloc/taxi_booking_event.dart';
-import 'package:taxi_app/bloc/taxi_booking_state.dart';
-import 'package:taxi_app/widgets/destination_selection_widget.dart';
-import 'package:taxi_app/widgets/home_app_bar.dart';
-import 'package:taxi_app/widgets/home_drawer.dart';
-import 'package:taxi_app/widgets/taxi_booking_confirmed_widget.dart';
-import 'package:taxi_app/widgets/taxi_booking_home_widget.dart';
-import 'package:taxi_app/widgets/taxi_map.dart';
+import 'package:carwash_app/bloc/carwash_booking_bloc.dart';
+import 'package:carwash_app/bloc/carwash_booking_event.dart';
+import 'package:carwash_app/bloc/carwash_booking_state.dart';
+import 'package:carwash_app/widgets/destination_selection_widget.dart';
+import 'package:carwash_app/widgets/home_app_bar.dart';
+import 'package:carwash_app/widgets/home_drawer.dart';
+import 'package:carwash_app/widgets/carwash_booking_confirmed_widget.dart';
+import 'package:carwash_app/widgets/carwash_booking_home_widget.dart';
+import 'package:carwash_app/widgets/carwash_map.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -28,7 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
-        BlocProvider.of<TaxiBookingBloc>(context).add(BackPressedEvent());
+        BlocProvider.of<CarwashBookingBloc>(context).add(BackPressedEvent());
         return false;
       },
       child: AnnotatedRegion<SystemUiOverlayStyle>(
@@ -36,20 +36,20 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Scaffold(
           endDrawer: HomeDrawer(),
           body: Stack(
-            children: <Widget>[TaxiMap(), HomeAppBar()],
+            children: <Widget>[CarwashMap(), HomeAppBar()],
           ),
-          bottomSheet: BlocBuilder<TaxiBookingBloc, TaxiBookingState>(
-              builder: (BuildContext context, TaxiBookingState state) {
-            if (state is TaxiBookingNotInitializedState) {
+          bottomSheet: BlocBuilder<CarwashBookingBloc, CarwashBookingState>(
+              builder: (BuildContext context, CarwashBookingState state) {
+            if (state is CarwashBookingNotInitializedState) {
               return Container();
             }
-            if (state is TaxiBookingNotSelectedState) {
+            if (state is CarwashBookingNotSelectedState) {
               return DestinationSelctionWidget();
             }
-            if (state is TaxiBookingConfirmedState) {
-              return TaxiBookingConfirmedWidget();
+            if (state is CarwashBookingConfirmedState) {
+              return CarwashBookingConfirmedWidget();
             }
-            return TaxiBookingHomeWidget();
+            return CarwashBookingHomeWidget();
           }),
         ),
       ),
